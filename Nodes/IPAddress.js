0ff69af7cfb359fcb95a508b7dd376b2d067d81d
Tuan_Anh <anhnt.2309@gmail.com>
2017-10-17 17:39:00 +0700

    use bonjour library to discover mDNS services

--- Nodes/IPAddress.js
+++ Nodes/IPAddress.js
@@ -1,41 +1,64 @@
 module.exports = function(RED) {
-    function rmpluginIPAddressConfig(config) {
-        RED.nodes.createNode(this, config);
-        this.ipaddress = config.ipaddress;
-        this.port = config.port
-    }
-    RED.nodes.registerType("rm-plugin-config", rmpluginIPAddressConfig);
+  function rmpluginIPAddressConfig(config) {
+    RED.nodes.createNode(this, config);
+    this.ipaddress = config.ipaddress;
+    this.port = config.port;
+    setInterval(mdnsScan,10000);
+  }
+
+  function mdnsScan() {
+    console.log('on scan');
+    var bonjour = require('bonjour')()
+
+// advertise an HTTP server on port 3000 
+// bonjour.publish({ name: 'My Web Server', type: 'http', port: 3000 })
+
+          // browse for all http services 
+          bonjour.find({  }, function (service) {
+            
+            for(key in service){
+               // console.log("key: "+ key);
+              if(key == 'type'){
+
+                var type = service[key];
+                  // console.log("key value: "+ service[key]);
+                if(type == "rmplugin-http"  ){
+                  // console.log('Found an HTTP server:', service);
+                       // console.log('data:', data);
+                    
+                       var address = service["addresses"];
+                       for(var value in address){
+                        var addressSplit = address[value].split(".");
+                        if(addressSplit.length === 4){
+                          var finalAddress = address[value];
+                        }
+                      }
+
+                       var port = service["port"];
+                       console.log('address:',finalAddress);
+                       console.log('port:',port);                    
+                      var configInfo = [];
+                      configInfo.push({
+                        key:address,
+                        value: finalAddress
+                      });
+                      configInfo.push({
+                        key:port,
+                        value: port
+                      });
+                      RED.httpAdmin.get('/config',function(req,res){
+                        res.json(configInfo || []);
+                      });
+                    
+                     }                 
+                   }
+                 }
+
+               })
 
 
+        }
 
-   //  function rmpluginIPAddress(config) {
-   //      RED.nodes.createNode(this,config);
-   //      var context = this.context();
-   //      // this.ipaddress = config.ipaddress;
-   //      // this.port = config.port;
-   //      var node = this;
-   // // Retrieve the config node
-   //      this.server = RED.nodes.getNode(config.server);
-
-   //      if (this.server) {
-   //          // Do something with:
-           
-   //          //  this.server.host
-   //          node.send(this.server.ipaddress);
-   //          //  this.server.port
-   //          node.send(this.server.port);
-   //          this.on('input', function(msg) {
-   //            var payloadString = "http://" + this.server.ipaddress + ":" + this.server.port;
-   //          msg.payload = payloadString;
-   //         msg.ipaddress = this.server.ipaddress;
-
-   //      node.send(msg);
-   //  });
-
-   //      } else {
-   //          // No config node configured
-   //      }
-   //  }
-   //  RED.nodes.registerType("RM Plugin IP Input",rmpluginIPAddress);
-
-};
+        
+        RED.nodes.registerType("rm-plugin-config", rmpluginIPAddressConfig);
+      };
