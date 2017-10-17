6489200d86539110bc7ac4630ca5b37ace9e1ea9
Tuan_Anh <anhnt.2309@gmail.com>
2017-10-17 12:33:53 +0700

    format code and filter devices with typecode < 80000

--- Nodes/RMPluginGetDevices.js
+++ Nodes/RMPluginGetDevices.js
@@ -1,36 +1,36 @@
 module.exports = function(RED) {
-    function rmpluginGetDevices(config) {
-        RED.nodes.createNode(this,config);
-        var context = this.context();
+  function rmpluginGetDevices(config) {
+    RED.nodes.createNode(this,config);
+    var context = this.context();
         // this.ipaddress = config.ipaddress;
         // this.port = config.port;
         var node = this;
         var msg;
    // Retrieve the config node
-        this.server = RED.nodes.getNode(config.server);
+   this.server = RED.nodes.getNode(config.server);
 
-        if (this.server) {
+   if (this.server) {
             // Do something with:
-           
+            
             //  this.server.host
             node.send(this.server.ipaddress);
             //  this.server.port
             node.send(this.server.port);
-             this.on('input', function(msg) {
-                node.status({fill:"blue",shape:"ring",text:"requesting"});
+            this.on('input', function(msg) {
+              node.status({fill:"blue",shape:"ring",text:"requesting"});
 
-            const http = require("http");
-               var url = "http://" + this.server.ipaddress + ":" + this.server.port+"/devices";
+              const http = require("http");
+              var url = "http://" + this.server.ipaddress + ":" + this.server.port+"/devices";
 
-            http.get(url, (resp) => {
+              http.get(url, (resp) => {
 
-              let rawData = '';
-             
+                let rawData = '';
+                
               // A chunk of data has been recieved.
               resp.on('data', (chunk) => {
                 rawData += chunk;
               });
-             
+              
               // The whole response has been received. Print out the result.
               resp.on('end', () => {
                 console.log(JSON.parse(rawData).explanation);
@@ -38,30 +38,32 @@ module.exports = function(RED) {
                 msg.payload = JSON.parse(rawData);
 
                 var options = [];
-              for (var i in msg.payload){
-                  var displayName = msg.payload[i].name;
-                  var value = msg.payload[i].mac;
-                  var optionsItem = {};
-                  optionsItem[displayName] = value;
-                  options.push(optionsItem);
-                 }
-               msg.options =options;
+                for (var i in msg.payload){
+                  if(msg.payload[i].typeCode < 80000){
+                    var displayName = msg.payload[i].name;
+                    var value = msg.payload[i].mac;
+                    var optionsItem = {};
+                    optionsItem[displayName] = value;
+                    options.push(optionsItem);
+                  }
+                }
+                msg.options =options;
 
 
                 node.send(msg);
                 node.status({fill:"green",shape:"dot",text:"completed"});
               });
-             
+              
             }).on("error", (err) => {
               console.log("Error: " + err.message);
               node.status({fill:"red",shape:"ring",text:"ERR: " + err.message});
             });
-        });
+          });
 
-        } else {
+          } else {
             // No config node configured
+          }
         }
-    }
-    RED.nodes.registerType("Get Device List",rmpluginGetDevices);
+        RED.nodes.registerType("Get Device List",rmpluginGetDevices);
 
-};
+      };
