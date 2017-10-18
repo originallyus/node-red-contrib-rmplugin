49c0a0720009c590ca22c130bfed9dae60e97425
Tuan_Anh <anhnt.2309@gmail.com>
2017-10-18 12:44:32 +0700

    add auto discover ipaddress and port function

--- Nodes/IPAddress.js
+++ Nodes/IPAddress.js
@@ -3,62 +3,44 @@ module.exports = function(RED) {
     RED.nodes.createNode(this, config);
     this.ipaddress = config.ipaddress;
     this.port = config.port;
-    setInterval(mdnsScan,10000);
+
+     //Build another API to auto detect IP Addresses
+     discoverIpAddresses('rmplugin-http', function(ipaddresses) {
+      RED.httpAdmin.get('/ipaddresses', function(req, res) {
+        res.json(ipaddresses);
+      });
+    });
+   };
+
+   function discoverIpAddresses(serviceType, discoveryCallback)
+   {
+    var configInfo = [];
+    var bonjour = require('bonjour')();
+    var browser = bonjour.find({type: serviceType}, function(service) {
+      service.addresses.forEach(function(element) {
+        if (element.split(".").length == 4) {
+          var label = "" + service.txt.md + " (" + element + ")";         
+          configInfo.push({
+            key:"address",
+            value: element
+          });
+       
+        }
+      });
+     
+      configInfo.push({
+        key:"port",
+        value: service.port
+      });
+      //Add a bit of delay for all services to be discovered
+      if (discoveryCallback) 
+        setTimeout(function(){ 
+          discoveryCallback(configInfo);
+        }, 2000);
+    });
+
+
   }
 
-  function mdnsScan() {
-    console.log('on scan');
-    var bonjour = require('bonjour')()
-
-// advertise an HTTP server on port 3000 
-// bonjour.publish({ name: 'My Web Server', type: 'http', port: 3000 })
-
-          // browse for all http services 
-          bonjour.find({  }, function (service) {
-            
-            for(key in service){
-               // console.log("key: "+ key);
-              if(key == 'type'){
-
-                var type = service[key];
-                  // console.log("key value: "+ service[key]);
-                if(type == "rmplugin-http"  ){
-                  // console.log('Found an HTTP server:', service);
-                       // console.log('data:', data);
-                    
-                       var address = service["addresses"];
-                       for(var value in address){
-                        var addressSplit = address[value].split(".");
-                        if(addressSplit.length === 4){
-                          var finalAddress = address[value];
-                        }
-                      }
-
-                       var port = service["port"];
-                       console.log('address:',finalAddress);
-                       console.log('port:',port);                    
-                      var configInfo = [];
-                      configInfo.push({
-                        key:address,
-                        value: finalAddress
-                      });
-                      configInfo.push({
-                        key:port,
-                        value: port
-                      });
-                      RED.httpAdmin.get('/config',function(req,res){
-                        res.json(configInfo || []);
-                      });
-                    
-                     }                 
-                   }
-                 }
-
-               })
-
-
-        }
-
-        
-        RED.nodes.registerType("rm-plugin-config", rmpluginIPAddressConfig);
-      };
+  RED.nodes.registerType("rm-plugin-config", rmpluginIPAddressConfig);
+};
