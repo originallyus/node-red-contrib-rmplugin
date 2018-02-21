c4065389b05d6c61bd6e553b1c7970f43de43ae1
Tuan_Anh <anhnt.2309@gmail.com>
2018-02-21 10:31:04 +0700

    clean up code

--- Nodes/RMPluginExecuteCommand.js
+++ Nodes/RMPluginExecuteCommand.js
@@ -1,55 +1,34 @@
 module.exports = function(RED) {
-    function rmpluginExecuteCommand(config) {
-        RED.nodes.createNode(this,config);
-        var context = this.context();
+  function rmpluginExecuteCommand(config) {
+    RED.nodes.createNode(this,config);
+    var context = this.context();
         // this.ipaddress = config.ipaddress;
         // this.port = config.port;
         var node = this;
         var msg;
    // Retrieve the config node
-        this.server = RED.nodes.getNode(config.server);
+   this.server = RED.nodes.getNode(config.server);
 
-        if (this.server) {
+   if (this.server) {
             // Do something with:
-           
+            
             //  this.server.host
             node.send(this.server.ipaddress);
             //  this.server.port
             node.send(this.server.port);
              this.on('input', function(msg) {
                 node.status({fill:"blue", shape:"ring", text:"requesting"});
+           
+              const http = require("http");
+              var url = "http://" + this.server.ipaddress + ":" + this.server.port+"/send?deviceMac="+msg.deviceMAC+"&codeId="+msg.codeID;
 
-            const http = require("http");
-            var url = "http://" + this.server.ipaddress + ":" + this.server.port+"/send?deviceMac="+msg.deviceMAC+"&codeId="+msg.codeID;
-
-            http.get(url, (resp) => {
-
-              const { statusCode } = resp;
-              const contentType = resp.headers['content-type'];
-
-              var error;
-              if (statusCode !== 200) {
-                error = new Error('Request Failed.\n' +
-                                  `Status Code: ${statusCode}`);
-              } else if (!/^application\/json/.test(contentType)) {
-                error = new Error('Invalid content-type.\n' +
-                                  `Expected application/json but received ${contentType}`);
-              }
-              if (error) {
-                console.error(error.message);
-                // consume response data to free up memory
-                resp.resume();
-                return;
-              }
-
-              resp.setEncoding('utf8');
-              
+              http.get(url, (resp) => {
               // A chunk of data has been received.
               var rawData = '';
               resp.on('data', (chunk) => {
                 rawData += chunk;
               });
-             
+              
               // The whole response has been received. Print out the result.
               resp.on('end', () => {
                   const { statusCode } = resp;
@@ -75,21 +54,22 @@ module.exports = function(RED) {
                   
                   var success = msg.rawResponse.status === "ok";
                   msg.payload = success ? "success" : "failed";
+                  console.log(msg.rawResponse);
                   
                   node.send(msg);
                   node.status({fill:"green", shape:"dot", text:success});
               });
-             
+                  
             }).on("error", (err) => {
               console.log("Error: " + err.message);
               node.status({fill:"red", shape:"ring", text:"error: " + err.message});
             });
-        });
+          });
 
-        } else {
+          } else {
             // No config node configured
+          }
         }
-    }
-    RED.nodes.registerType("Execute Command",rmpluginExecuteCommand);
+        RED.nodes.registerType("Execute Command",rmpluginExecuteCommand);
 
-};
+      };
